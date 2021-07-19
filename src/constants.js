ba3f230de4af588d3212aaf07c24c7c5b1fca131
Wilson Kiggundu <wkiggundu@innovationvillage.co.ug>
2021-07-19 13:48:56 +0300

    Updated API BASE URL

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.API_BASE_URL || "https://localhost:2001/api/responses"
+export const API_BASE_URL = "https://api.devscape.myvillage.africa/api/responses"
 export const COLORS = ['#aa00ff', '#ffe7a9', '#00bfa5']
