251899257e43dda21057c51dd706d59e7ad864fb
Wilson Kiggundu <wkiggundu@innovationvillage.co.ug>
2021-09-13 19:50:43 +0300

    Added google analytics

--- src/hooks/useTracking.ts
+++ src/hooks/useTracking.ts
@@ -12,21 +12,21 @@ declare global {
 }
 
 export const useTracking = (trackingId: string) => {
-    // const {listen} = useHistory()
-    //
-    // useEffect(() => {
-    //     return listen((location:any) => {
-    //         if (!window.gtag) return
-    //         if (!trackingId) {
-    //             console.log(
-    //                 'Tracking not enabled, as `trackingId` was not given and ' +
-    //                 'there is no `GA_MEASUREMENT_ID`.'
-    //             )
-    //             return
-    //         }
-    //
-    //         window.gtag('config', trackingId, { page_path: location.pathname })
-    //     })
-    //
-    // }, [trackingId, listen])
+    const {listen} = useHistory()
+
+    useEffect(() => {
+        return listen((location:any) => {
+            if (!window.gtag) return
+            if (!trackingId) {
+                console.log(
+                    'Tracking not enabled, as `trackingId` was not given and ' +
+                    'there is no `GA_MEASUREMENT_ID`.'
+                )
+                return
+            }
+
+            window.gtag('config', trackingId, { page_path: location.pathname })
+        })
+
+    }, [trackingId, listen])
 }
